e5fc7fcabeecc410250a3be5f0802ed6d015ce56
Sarthak Rana <rsarthak2845@gmail.com>
2025-06-09 18:31:59 +0530

    fix: resolve merge conflicts in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
     return <Preloader onFinish={() => setLoading(false)} />;
   }
   return (
-<<<<<<< HEAD
     <>
   <div className="fixed inset-0 z-0">
     <StarBackground />
@@ -20,18 +19,6 @@ const App = () => {
     <p className="mt-4 text-lg">This is a simple React application.</p>
   </div>
 </>
-=======
-    <>  
-     <StarBackground /> 
-    
-  
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-white">
-        <h1 className="text-4xl font-bold">Welcome to StarGazeX</h1>
-        <p className="mt-4 text-lg">This is a simple React application.</p>
-         <UserProfile />
-      </div>
-    </>
->>>>>>> fb34b275419b2d12ed1620d5bdcca03985a6a883
   );
 };
 

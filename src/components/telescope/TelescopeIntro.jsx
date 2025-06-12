e9cb306e7c6c8dd6f2631af5d1a7ce097f4e1255
Yash Pakhare <cs23bt011@iitdh.ac.in>
2025-06-12 16:14:50 +0530

    Fixed padding in Telescope

--- src/components/telescope/TelescopeIntro.jsx
+++ src/components/telescope/TelescopeIntro.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const TelescopeIntro = () => {
   return (
     <motion.div 
-      className="bg-none p-6 rounded-xl shadow-md relative"
+      className="bg-none pb-6 rounded-xl shadow-md relative"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5, delay: 0.2 }}>
